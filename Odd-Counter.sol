0ac35e7b96ddb8cdbcbd1cd79d0cae9f00d52cd6
Eugene Fuller <32392316+Cryptogenysis@users.noreply.github.com>
2022-06-05 16:36:10 -0700

    Create Odd-Counter.sol

---
+++ Odd-Counter.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract oddCounter {
+uint public count = 1;
+
+function increment() public returns(uint) {
+count += 2;
+return count;
+}
+
+function decrement() public returns(uint) {
+count -= 2;
+return count;
+}
+
+}
