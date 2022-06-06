48e9614c1a5332b858d5d8ef0a778fc60c14d66c
Eugene Fuller <32392316+Cryptogenysis@users.noreply.github.com>
2022-06-05 18:10:59 -0700

    Update Odd-Counter.sol
    negative integer bug fix

--- Odd-Counter.sol
+++ Odd-Counter.sol
@@ -3,16 +3,16 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract oddCounter {
-uint public count = 1;
+int public count = 1;
 
-function increment() public returns(uint) {
+function increment() public returns(int) {
 count += 2;
 return count;
 }
 
-function decrement() public returns(uint) {
+function decrement() public returns(int) {
 count -= 2;
-return count;
+return count;`
 }
 
 }
